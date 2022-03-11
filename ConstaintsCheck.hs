3ba69144e549f0b1911a6a32f1e1b6c9c3d6ea49
Zhezhee <yuzhe.zhou@ucalgary.ca>
2022-03-10 19:52:46 -0700

    module && error print

--- ConstaintsCheck.hs
+++ ConstaintsCheck.hs
@@ -1,32 +1,38 @@
---check 8 lines, 8 columns 
---check number greater than 0
---check it is a number instead of other things
-
+module ConstainsCheck(
+    checkName,
+    checkFormat,
+    checkTooNearP,
+    checkMachinePenalty,
+    letterCheck,
+    numCheck,
+    errorPrint
+)where
+    
 --Name
-formatCheck1 :: String -> Bool
-formatCheck1 [] = False
-formatCheck1 x
+checkName :: String -> Bool
+checkName [] = False
+checkName x
     | not(null x) && (' ' `notElem` x) =True
     | otherwise = False
 
 --forced partial && forbidden machine && too-near Tasks
-formatCheck2 :: String -> Bool
-formatCheck2 [] = False
-formatCheck2 x
+checkFormat :: String -> Bool
+checkFormat [] = False
+checkFormat x
     | (head x == '(') && (last x == ')') && (x!!2 == ',') && (length x == 5)= True
     | otherwise = False
 
 --too-near Penalities
-formatCheck3 :: String -> Bool
-formatCheck3 [] = False
-formatCheck3 x
+checkTooNearP :: String -> Bool
+checkTooNearP [] = False
+checkTooNearP x
     | (head x == '(') && (last x == ')') && (x!!2 == ',') && (x!!4== ',') && (length x == 7)= True
     | otherwise = False
 
 --machine penalties
-formatCheck5 :: String -> Bool
-formatCheck5 [] = False 
-formatCheck5 x 
+checkMachinePenalty :: String -> Bool
+checkMachinePenalty [] = False 
+checkMachinePenalty x 
     | (length x == 15) && (x!!1 == ' '&&x!!3 == ' '&& x!!5 == ' '&& x!!7 == ' ' && x!!9 == ' '&& x!!11 == ' '&& x!!13 == ' ') = True 
     | otherwise = False
 
@@ -40,3 +46,15 @@ numCheck :: Int -> Bool
 numCheck x 
     | x `elem` [1..8] = True 
     | otherwise = False
+
+--Output Errors
+errorPrint :: Int -> String
+errorPrint warning
+    | warning == 0 = "Name Error"
+    | warning == 1 = "partial assignment error"
+    | warning == 2 = "invalid machine/task"
+    | warning == 3 = "machine penalty error"
+    | warning == 4 = "invalid task"
+    | warning == 5 = "invalid penalty"
+    | warning == 6 = "Error while parsing input file"
+    | otherwise = ""
