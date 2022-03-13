ec4f448e63a06dd7424b798ebbe667142ae69ee4
XinzhouLi <48045064+XinzhouLi@users.noreply.github.com>
2022-03-12 18:42:43 -0700

    Update ConstraintConstr.hs

--- ConstraintConstr.hs
+++ ConstraintConstr.hs
@@ -4,3 +4,14 @@ tooNearConstr _ = tooNearConvert [[]]
 
 tooNearConvert :: [String] -> [[Bool]] -> [[Bool]]
 tooNearConvert _ x = x cycle 8 []
+
+
+--machine Pens 8*8 matrix builder
+machinePensConstr :: [String] ->  [[Int]] -> [[Int]]
+machinePensConstr [x] matrix = matrix ++ [listNumConver (words x) []]
+machinePensConstr (x:xs) matrix = machinePensConstr xs (matrix ++ [listNumConver (words x) []])
+
+
+listNumConver :: [String] -> [Int] -> [Int]
+listNumConver [x] intList = intList ++ [read x + 0]
+listNumConver (x:xs) intList = listNumConver xs (intList ++ [read x + 0])
